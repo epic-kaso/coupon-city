b20dcdec354cdae227a44b1bdf5f7a212cc10272
Akachukwu Okafor <kasoprecede47@gmail.com>
2014-07-21 21:34:23 +0100

    just a bug fix

--- application/config/facebook.php
+++ application/config/facebook.php
@@ -1,7 +1,10 @@
-<?php if (!defined('BASEPATH')) exit('No direct script access allowed');
+<?php
+
+if (!defined('BASEPATH')) {
+    exit('No direct script access allowed');
+}
 
 
-$config['appId']   = 'APP_ID_HERE';
-$config['secret']  = 'SECRET_HERE';
-
+$config['appId'] = '1413599752212245';
+$config['secret'] = '8d024f2b7d57488541a56e1e279b5bea';
 ?>
