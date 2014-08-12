90e1a98490a58365bf24a27f6862802b0eecb249
Larry Oti <larryhomie@gmail.com>
2014-08-12 12:27:54 -0700

    merchant nav header conflict resolve

--- application/views/partials/merchant/_header_nav.php
+++ application/views/partials/merchant/_header_nav.php
@@ -2,11 +2,8 @@
     <div class="center clearfix">
         <span class="_logo left"></span>
         <div class="merchant-nav left">
-<<<<<<< HEAD
             <a href="<?= base_url('merchant/dashboard'); ?>">Dashboard</a>
-=======
-            <a href="<?= base_url(Merchant::MERCHANT_URL); ?>">Dashboard</a>
->>>>>>> b10b6cc9a4b679c0c03b8296dc54820be651ca7f
+
             <a href="<?= base_url('merchant/my-coupons'); ?>">Coupons</a>
             <a href="<?= base_url('merchant/redeem-coupon'); ?>" id="redeem">Redeem</a>
             <a href="<?= base_url(Merchant::MERCHANT_URL . '/profile'); ?>">Business</a>
@@ -20,13 +17,9 @@
                 </li>
             <?php } else { ?>
                 <li class="left">
-<<<<<<< HEAD
                     <a href="<?= base_url('merchant/settings'); ?>">
                         <?= $merchant->business_name ?>
                     </a>
-=======
-                    <?= $merchant->business_name ?>
->>>>>>> b10b6cc9a4b679c0c03b8296dc54820be651ca7f
                 </li>
                 <li class="left _logout">
                     <a href="<?= base_url(Merchant::MERCHANT_URL . '/logout'); ?>">&raquo; &nbsp;Logout</a>
@@ -36,9 +29,4 @@
 
     </div>
 </div>
-<<<<<<< HEAD
 
-
-
-=======
->>>>>>> b10b6cc9a4b679c0c03b8296dc54820be651ca7f
