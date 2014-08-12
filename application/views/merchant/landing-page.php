c9d5a204192e34acbdbd929857ec6f5fa48b2e90
Larry Oti <larryhomie@gmail.com>
2014-08-11 20:30:55 -0700

    landing page complete

--- application/views/merchant/landing-page.php
+++ application/views/merchant/landing-page.php
@@ -1,18 +1,83 @@
-<div class="container" style="margin-top: 100px;">
-    <div class="jumbotron">
-        <h1>Welcome to Merchant page.</h1>
-    </div>
-    <div class="row">
-        <div class="offset3 span6">
-            <div class="span3">
-                <label>Already have an Account?</label>
-                <a href="<?= base_url('merchant/login') ?>" >Login</a>
-            </div>
 
-            <div class="span3">
-                <label>Don't have an account?</label>
-                <a href="<?= base_url('merchant/signup') ?>" >Create Account</a>
+<div class="merchant-landing-page">
+
+    <div class="hero-banner">
+        <div class="hero-touch2">
+            <div class="hero-touch">
+                <div class="_header center clearfix">
+                    <div class="left">
+                        <a href="" class="landing-logo"></a>
+                        <a href="">Why CouponCity</a>
+                        <a href="">Pricing</a>
+                        <a href="">Contact Us</a>
+                    </div>
+                    
+                    <div class="right">
+                        <a href="<?= base_url('merchant/login') ?>" class="btn login__">Login</a>
+                        <a href="<?= base_url('merchant/signup') ?>" class="btn">Create Account</a>
+                    </div>
+                </div>
+
+                <div class="center">
+                    <h1>Get more customers to your business</h1>
+                    <p>Drive lots of customers to your business both online and offline by offering discount coupons. Use the tools included in our platform to measure, track and gain valuable insights on your business. It's a win-win for everyone. </p>
+                    <a href="<?= base_url('merchant/signup') ?>" class="btn-trans">Get Started Now</a>
+                </div>
             </div>
+        </div>
+    </div>
+
+    <div class="top-header top-banner-landing"></div>
+
+    <div class="intro">
+        <ol class="center clearfix">
+            <li class="left">
+                <h2>More customers</h2>
+                <p>We drive more customers to yur door and with the experience we try to sell, many are going to be repeat customers with potential of sharing and dragging their friends along.</p>
+            </li>
+
+            <li class="left">
+                <h2>More Money</h2>
+                <p>We are all in business for the money and we are building a never ending source of revenue for your business. You can only make money and make more of it.</p>
+            </li>
+
+            <li class="right">
+                <h2>Better Insights</h2>
+                <p>With the tools on our platform, you have access to insights and performance data. Where your customers are coming from and the type of deals they purchase alongside yours.</p>
+            </li>
+
+        </ol>
+    </div>
+
+    <div class="landing-pricing" style="background:#0f75bc;color:#fff;">
+        <div class="center">
+            <h1>Pricing? <span class="free-price">FREE!</span></h1>
+            
+            <h2><span>₦</span>0</h2>
+            <p>There is no sign on fee. We only make money when you sell. We are commited to being open. Our margins are small, you won't even notice.</p>
+        </div>
+    </div>
+
+    <div class="landing-us">
+        <div class="center">
+            <br/>
+            <h1>Business is fun. Drive sales. Make money</h1>
+
+            <a href="<?= base_url('merchant/signup') ?>" class="btn-trans">Get Started Now</a>
+
+            <p><strong>Phone:</strong> 07012345678</p>
+            <p><strong>Email:</strong> merchant@couponcity.ng</p>
+            <p><strong>Support Service:</strong> support@couponcity.ng</p>
+        </div>
+    </div>
+
+    <div class="landing-footer">
+        <div class="center">
+            &copy; 2014 Couponcity, NG. 
+            <a href="">Terms</a>
+            <a href="">Privacy</a>
+            <a href="<?= base_url('help-faq') ?>">Help</a>
+            <a href="<?= base_url('contact') ?>">Contact</a>
         </div>
     </div>
 </div>
