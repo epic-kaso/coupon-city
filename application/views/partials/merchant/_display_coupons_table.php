d6209bd605405c078b810ccc71d20105577f99fd
Akachukwu Okafor <kasoprecede47@gmail.com>
2014-08-22 16:19:06 +0100

    modifyinh merchant password reset

--- application/views/partials/merchant/_display_coupons_table.php
+++ application/views/partials/merchant/_display_coupons_table.php
@@ -1,78 +1,45 @@
-<table>
-    <thead>
-        <tr>
-            <td>Name</td>
-            <td>Summary</td>
-            <td>Active</td>
-            <td>Start Date</td>
-            <td>End Date</td>
-            <td>&nbsp;</td>
-        </tr>
-    </thead>
-
-    <tbody>
+        <?php
+        $items = $coupons->items();
+        if(count($items) == 1 && property_exists($items[0], 'empty') ){
+            ?>
+            <div class="if-no-deal">
+                <div class="no-deal no-coupons"></div>
+                <p>You currently have no Coupons</p>
+                <p>Give discounts and drive customers to your business. We are here to help you. <a href="">Start here</a></p>
+            </div>
+        <?php
+        }else{ ?>
+        <table>
+            <tr>
+                <th>Date</th>
+                <th>ID</th>
+                <th>Coupon Name</th>
+                <th>State</th>
+            </tr>
         <?php
         foreach ($coupons->items() as $coupon) {
-            if (property_exists($coupon, 'empty')) {
+        ?>
+        <tr>
+            <td><?= $coupon->created_at ?></td>
+            <td><?= $coupon->coupon_code ?></td>
+            <td class="align-left">
+                <a href='<?= base_url(Merchant::MERCHANT_URL . '/my-coupon/' . $coupon->slug) ?>'>
+                    <?= $coupon->name ?></a>
+            </td>
+            <td>
+                <?=
+                $coupon->deal_status == 1 ? '<span class="trans-state verified-trans">Active</span>' :
+                    '<span class="trans-state pending-trans">Inactive</span>';
                 ?>
-                <tr>
-                    <td colspan="6"><?= $coupon->name ?></td>
-                </tr>
-            <?php } else {
-                ?>
-                <tr>
-                    <td><?= $coupon->name ?></td>
-                    <td><?= $coupon->summary ?></td>
-                    <td><?= $coupon->deal_status == 1 ? 'Active' : 'Inactive'; ?></td>
-                    <td><?= $coupon->start_date ?></td>
-                    <td><?= $coupon->end_date ?></td>
-                    <td><a href='<?= base_url(Merchant::MERCHANT_URL . '/my-coupon/' . $coupon->slug) ?>'>Details..</a></td>
-                </tr>
-                <?php
+            </td>
+        </tr>
+        <?php
             }
+          ?>
+        </table>
+       <?php
         }
         ?>
-    </tbody>
-</table>
-
-<!---->
-
-<div class="if-no-deal">
-    <div class="no-deal no-coupons"></div>
-    <p>You currently have no Coupons</p>
-    <p>Give discounts and drive customers to your business. We are here to help you. <a href="">Start here</a></p>
-</div> 
-
-
-<table>
-    <tr>
-        <th>Date</th>
-        <th>ID</th>
-        <th>Coupon Name</th>
-        <th>State</th>
-    </tr>
-
-    <tr>
-        <td>12 May 3:20pm</td>
-        <td>34567890DFFC</td>
-        <td class="align-left"><a href="">The Chicken Pie Deal 50% Off</a></td>
-        <td><span class="trans-state verified-trans">Active</span></td>
-    </tr>
-
-    <tr>
-        <td>12 May 3:20pm</td>
-        <td>34567890DFFC</td>
-        <td class="align-left"><a href="">The Chicken Pie Deal 50% Off</a></td>
-        <td><span class="trans-state pending-trans">Pending</span></td>
-    </tr>
-
-    <tr>
-        <td>12 May 3:20pm</td>
-        <td>34567890DFFC</td>
-        <td class="align-left"><a href="">The Chicken Pie Deal 50% Off</a></td>
-        <td><span class="trans-state cancelled-trans">Completed</span></td>
-    </tr>
-</table>
 
 
 
