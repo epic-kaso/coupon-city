e563548e2b9b429f09a9a1272ed09e66f7470c1e
Akachukwu Okafor <kasoprecede47@gmail.com>
2014-08-30 20:13:50 +0100

    message

--- application/views/layouts/email/application.php
+++
@@ -1,9 +0,0 @@
-<?php
-echo partial('partials/_header', array('title' => 'Couponcity'));
-?>
-<div ng-app="endUserApp">
-    <?= $yield ?>
-</div>
-<?php
-echo partial('partials/_footer', array('app_type' => 'user'));
-?>
